b0fa739788333d166aa6a68e8e14b796e5238e97
Gustavo Michels de Camargo <guga.power@hotmail.com>
2022-10-27 22:10:25 -0300

    Updates

---
+++ precache-manifest.5dcba07d11c8922b2b5d4c505578a58a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e97eb64daf0a86aaab65e631af1732",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4edffb6f48abb7815de9",
+    "url": "/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "8195c18a30d0afb92723",
+    "url": "/static/css/main.e8901382.chunk.css"
+  },
+  {
+    "revision": "4edffb6f48abb7815de9",
+    "url": "/static/js/2.bd689159.chunk.js"
+  },
+  {
+    "revision": "45dd6a8eb784f4bfc84b6e54cbce993a",
+    "url": "/static/js/2.bd689159.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8195c18a30d0afb92723",
+    "url": "/static/js/main.e8fc8796.chunk.js"
+  },
+  {
+    "revision": "fddc7802fe391536fd78",
+    "url": "/static/js/runtime-main.d1d75a47.js"
+  }
+]);
