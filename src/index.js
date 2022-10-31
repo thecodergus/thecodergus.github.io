2248af3f2cad1f110dcd00fb1274b90234f33707
Gustavo Michels de Camargo <guga.power@hotmail.com>
2022-10-31 11:41:21 -0300

    Deixando algumas coisas pra uma futura melhor implementação de troca de idiomas

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {createRoot} from "react-dom/client"
+import { createStore } from "state-pool"
 
 /* GLOBAL VARIABLES */
+const store = createStore()
+store.setState("language", [
+    {
+        iconId: "primary-lang-icon",
+        language: "en",
+        classFlag: "twemoji-flag-for-flag-united-kingdom"
+    },
+    {
+        iconId: "secondary-lang-icon",
+        language: "pt-br",
+        classFlag: "twemoji-flag-for-flag-brazil"
+    }
+])
 
 window.$primaryLanguage = 'en';
 window.$secondaryLanguage = 'pt-br';
 window.$primaryLanguageIconId = 'primary-lang-icon';
 window.$secondaryLanguageIconId = 'secondary-lang-icon';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App />)
+
 serviceWorker.register();
