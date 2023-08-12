91f136b650bd2c334c4f570c0d268a39525b8d19
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-08-11 21:06:37 -0300

    Removendo os arquivos fora do que deve ser

--- src/config/localStore.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from "state-pool"
-
-// Docs
-// https://yezyilomo.github.io/state-pool/docs/basic_concepts/using_store_state
-const store = createStore()
-
-export default store
