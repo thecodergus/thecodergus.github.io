8d7ba54bad3079933394f62e2a1b72a26102d248
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2023-10-12 22:01:54 -0300

    Save point

--- src/components/games/doom1.jsx
+++ src/components/games/doom1.jsx
@@ -1,8 +1,37 @@
 // em um arquivo separado, por exemplo, Doom1.js
-import React from "react";
+import React, {useRef, useEffect, useState} from "react";
+import { DosPlayer as Instance, DosPlayerFactoryTyp } from "js-dos";
 
-const Doom1 = () => {
-  return <div>Welcome to Doom1 Game</div>;
+
+const Doom1 = (props) => {
+  const rootRef = useRef<HTMLDivElement>(null);
+      const [dos, setDos] = useState<Instance | null>(null);
+
+    useEffect(() => {
+        if (rootRef === null || rootRef.current === null) {
+            return;
+        }
+
+        const root = rootRef.current;
+        const instance = DosPlayerFactoryTyp(root);
+
+        setDos(instance);
+
+        return () => {
+            instance.stop();
+        };
+    }, [rootRef]);
+
+    useEffect(() => {
+        if (dos !== null) {
+            dos.run(props.bundleUrl); // ci is returned
+        }
+    }, [dos, props.bundleUrl]);
+
+
+  return (
+    <div ref={rootRef} style={{ width: "100%", height: "100%" }}></div>
+  );
 };
 
 export default Doom1;
